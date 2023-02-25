8a2d6ea7d431fa7958c7695589fe106c74ee8c7c
Lamia-Abdullah <lamia-albaqami@outlook.com>
2023-02-25 21:20:22 +0300

    ui done

---
+++ information/data.js
@@ -0,0 +1,34 @@
+export const projects = [
+  {
+    title: "nabataty",
+    subtitle: "TypeScript ,React , Css ,Html , Firebase and Vercel",
+    description:
+      "Online store for plants and their supplies",
+    image: "./project-1.gif",
+    link: "https://github.com/Lamia-Abdullah/nabataty-1",
+  },
+  {
+    title: "Tracker Marketing Stocks",
+    subtitle: "TypeScript, Fastify, Prisma, MongoDB and Heroku for deployment and frontend used cli",
+    description:
+      "Allows the user to check their portfolio any time",
+    image: "./project-2.gif",
+    link: "https://github.com/Lamia-Abdullah/stocks-cli",
+  },
+
+];
+
+
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "TypeSecript",
+  "Bootstrap",
+  "HTML",
+  "CSS",
+  "Flutter",
+
+
+];
